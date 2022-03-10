38e7a0edbecae300b0a9f60db225200fe259e224
Eliwuu <marek.wilk@gmail.com>
2022-03-10 22:31:00 +0100

    Add Register Endpoint

--- RLBenchmark.Api/Program.cs
+++ RLBenchmark.Api/Program.cs
@@ -18,22 +18,25 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-
-app.MapPost("/login", (new {string email, string password }) )
-
-app.MapGet("/weatherforecast", () =>
+app.MapPost("register", (RLBenchmark.Models.User.RegisterDto registerDto) =>
 {
-    var forecast = Enumerable.Range(1, 5).Select(index =>
-       new WeatherForecast
-       (
-           DateTime.Now.AddDays(index),
-           Random.Shared.Next(-20, 55),
-           summaries[Random.Shared.Next(summaries.Length)]
-       ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast");
+
+});
+//app.MapPost("/login", (new {string email, string password }) )
+
+//app.MapGet("/weatherforecast", () =>
+//{
+//    var forecast = Enumerable.Range(1, 5).Select(index =>
+//       new WeatherForecast
+//       (
+//           DateTime.Now.AddDays(index),
+//           Random.Shared.Next(-20, 55),
+//           summaries[Random.Shared.Next(summaries.Length)]
+//       ))
+//        .ToArray();
+//    return forecast;
+//})
+//.WithName("GetWeatherForecast");
 
 app.Run();
 
